1258db7bb27e2b300fc014cde83e017a4e9c422a
juancaruizc <juancruizcruz@gmail.com>
2021-03-10 16:02:01 -0700

    mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,77 @@
+const User = require('../users/users-model');
+// const UserPost = require('../posts/posts-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
 }
 
-function validateUserId(req, res, next) {
+const validateUserId = async (req, res, next) => {
   // DO YOUR MAGIC
-}
+  const { id } = req.params;
+  try {
+    const user = await User.getById(id);
+    if (!user) {
+      res.status(404).json({ message: 'user not found' });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+// const validateUserPostId = async (req, res, next) => {
+//   // DO YOUR MAGIC
+//   const { id } = req.params;
+//   try {
+//     const user = await UserPost.getById(id);
+//     if (!user) {
+//       res.status(404).json({ message: 'user not found' });
+//     } else {
+//       req.user_id = user;
+//       next();
+//     }
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if (!req.body) {
+    res.status(400).json({ message: 'missing user data' });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: 'missing required name field' });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const text = req.body.text;
+  if (!req.body) {
+    res.status(400).json({ message: 'missing post data' });
+  } else if (!text) {
+    res.status(400).json({ message: 'missing required text field' });
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+  // validateUserPostId,
+};
--- api/server.js
+++ api/server.js
@@ -7,8 +7,10 @@ server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
 const usersRouter = require('./users/users-router');
+const postsRouter = require('./posts/posts-router');
 
 server.use('/api/users', usersRouter);
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
